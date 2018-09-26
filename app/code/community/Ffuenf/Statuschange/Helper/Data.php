bc3e11cb02b11991d502ca7b6b4c84176d79c1dd
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2018-09-26 14:19:33 +0200

    * update travis * fix logging configuration and tests

--- app/code/community/Ffuenf/Statuschange/Helper/Data.php
+++ app/code/community/Ffuenf/Statuschange/Helper/Data.php
@@ -18,10 +18,10 @@
  */
 class Ffuenf_Statuschange_Helper_Data extends Ffuenf_Common_Helper_Core
 {
-    /**
-     * Path for the config for extension active status.
-     */
-    const CONFIG_EXTENSION_ACTIVE = 'ffuenf_statuschange/general/enable';
+    const CONFIG_EXTENSION_ACTIVE       = 'ffuenf_statuschange/general/enable';
+    const XML_PATH_LOG_SYSTEM_ACTIVE    = 'ffuenf_statuschange/log/enable';
+    const XML_PATH_LOG_PROFILE_ACTIVE   = 'ffuenf_statuschange/log/profile_enable';
+    const XML_PATH_LOG_EXCEPTION_ACTIVE = 'ffuenf_statuschange/log/exception_enable';
 
     /**
      * Variable for if the extension is active.
@@ -38,5 +38,35 @@ class Ffuenf_Statuschange_Helper_Data extends Ffuenf_Common_Helper_Core
     public function isExtensionActive()
     {
         return $this->getStoreFlag(self::CONFIG_EXTENSION_ACTIVE, 'bExtensionActive');
+    }
+
+    /**
+     * Check to see if logging is active.
+     *
+     * @return bool
+     */
+    public function isLogActive()
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_LOG_SYSTEM_ACTIVE);
+    }
+
+    /**
+     * Check to see if profile logging is active.
+     *
+     * @return bool
+     */
+    public function isLogProfileActive()
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_LOG_PROFILE_ACTIVE);
+    }
+
+    /**
+     * Check to see if exception logging is active.
+     *
+     * @return bool
+     */
+    public function isLogExceptionActive()
+    {
+        return Mage::getStoreConfigFlag(self::XML_PATH_LOG_EXCEPTION_ACTIVE);
     }
 }
--- app/code/community/Ffuenf/Statuschange/Test/Helper/Data.php
+++ app/code/community/Ffuenf/Statuschange/Test/Helper/Data.php
@@ -25,6 +25,21 @@
 class Ffuenf_Statuschange_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
 {
     /**
+     * @var Ffuenf_Statuschange_Helper_Data
+     */
+    protected $_helper;
+
+    public function setUp()
+    {
+        $this->_helper = new Ffuenf_Statuschange_Helper_Data();
+    }
+
+    public function tearDown()
+    {
+        $this->_helper = null;
+    }
+
+    /**
      * Tests is extension active.
      *
      * @test
@@ -35,6 +50,48 @@ class Ffuenf_Statuschange_Test_Helper_Data extends EcomDev_PHPUnit_Test_Case
         $this->assertTrue(
             Mage::helper('ffuenf_statuschange')->isExtensionActive(),
             'Extension is not active please check config'
+        );
+    }
+
+    /**
+     * Tests whether extension logging is active.
+     *
+     * @test
+     * @covers Ffuenf_Statuschange_Helper_Data::isLogActive
+     */
+    public function testIsLogActive()
+    {
+        $this->assertTrue(
+            $this->_helper->isLogActive(),
+            'System logging is not active please check config'
+        );
+    }
+
+    /**
+     * Tests whether extension profile logging is active.
+     *
+     * @test
+     * @covers Ffuenf_Statuschange_Helper_Data::isLogProfileActive
+     */
+    public function testIsLogProfileActive()
+    {
+        $this->assertTrue(
+            $this->_helper->isLogProfileActive(),
+            'Profile logging is not active please check config'
+        );
+    }
+
+    /**
+     * Tests whether extension exception logging is active.
+     *
+     * @test
+     * @covers Ffuenf_Statuschange_Helper_Data::isLogExceptionActive
+     */
+    public function testIsLogExceptionActive()
+    {
+        $this->assertTrue(
+            $this->_helper->isLogExceptionActive(),
+            'Exception logging is not active please check config'
         );
     }
 }
